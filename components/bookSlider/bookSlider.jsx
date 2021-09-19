0e9bfe2c351074d87520636c28ed80b4a34c72bf
peyman ahmadian <ahmadian.peyman@live.com>
2021-09-19 19:02:01 +0430

    setup slider

--- components/bookSlider/bookSlider.jsx
+++ components/bookSlider/bookSlider.jsx
@@ -1,8 +1,10 @@
+import Image from "next/image";
 import Slider from "react-slick";
 import styles from "./bookSlider.module.scss";
 import arrow from "../../public/assets/img/arrow_black.svg";
-import Image from "next/image";
 import {Book} from "./../../components";
+
+import icon from "./category.svg";
 const BookSlider=(props)=>{
     const NextArrowBtn=(props)=>{
         const {className,style,onClick}=props;
@@ -25,14 +27,21 @@ const BookSlider=(props)=>{
         infinite: true,
         speed: 500,
         slidesToShow: 5,
-        slidesToScroll: 1,
+        slidesToScroll: 5,
         nextArrow:<NextArrowBtn/>,
         prevArrow:<PrevArrowBtn/>
     }
     return(
-        <Slider className={styles.slider} {...setting}>
-            {props.items.map(item=><Book key={item.link} name={item.name} image={item.image} link={item.link}/>)}
-        </Slider>
+        <div className={styles.sliderContainer}>
+            <div className={styles.title}>
+                <Image src={icon} height={32} width={32} alt={props.title}/> 
+                <div>{props.title}</div>
+            </div>
+            <Slider className={styles.slider} {...setting}>
+                {props.items.map(item=><Book key={item.link} name={item.name} image={item.image} link={item.link}/>)}
+            </Slider>
+        </div>
+
     )
 }
 export default BookSlider;
