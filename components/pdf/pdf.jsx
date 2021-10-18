c3ea44678ee1abc26cff1e957ecdc6c9306fbf0c
Ahmadian Peyman <P.Ahmadian@Rayan.LAN>
2021-10-18 14:37:30 +0330

    setup PDF and EPUB view

---
+++ components/pdf/pdf.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import styles from "./pdf.module.scss";
+import { Document, Page, pdfjs } from "react-pdf";
+import workerSrc from "../../pdf-worker";
+pdfjs.GlobalWorkerOptions.workerSrc = workerSrc;
+export default function PDFViewer(props) {
+    const [numPages, setNumPages] = useState(null);
+    const [pageNumber, setPageNumber] = useState(1);
+    const  onDocumentLoadSuccess=({ numPages: nextNumPages })=> {
+        setNumPages(nextNumPages);
+    }
+
+    return (
+            <div className={styles.pdfContainer}>
+                <Document file={props.url} onLoadSuccess={onDocumentLoadSuccess}>
+                        <Page
+                            pageNumber={pageNumber}
+                        />
+                </Document>
+                <div className={styles.pageContainer}>
+                    <p>صفحه {pageNumber} از {numPages}</p>
+                    <button disabled={pageNumber===1} onClick={()=>setPageNumber(pageNumber-1)}>صفحه قبلی</button>
+                    <button disabled={pageNumber===numPages} onClick={()=>setPageNumber(pageNumber+1)}>صفحه بعدی</button>
+
+                </div>
+
+            </div>
+    );
+}
